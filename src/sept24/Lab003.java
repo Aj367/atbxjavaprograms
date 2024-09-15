c729425aff58b01c6b622fb45036688f9365850b
Mansan <gitac164367@gmail.com>
2024-09-15 17:06:41 +0530

    Latest files added and modified

--- src/sept24/Lab003.java
+++ src/sept24/Lab003.java
@@ -1,4 +1,16 @@
 package sept24;
 
 public class Lab003 {
+    public static void main (String[] args)
+    {
+        System.out.println("HelloWorld");
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("Hello_world");
+        System.out.println("Hello-World");
+        System.out.println("AtBx");
+        System.out.println("Sun");
+
+
+    }
 }
--- src/sept24/Lab004.java
+++ src/sept24/Lab004.java
@@ -1,4 +1,9 @@
 package sept24;
 
-public class Lab004 {
+public class Lab004
+{
+    public static void main (String[] args)
+    {
+    System.out.println("This is Lab004");
+    }
 }
