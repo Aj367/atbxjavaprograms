e17b60ac6589e015235a6c59f48221738475f82e
Mansan <gitac164367@gmail.com>
2024-09-16 10:51:22 +0530

    Added 2 files

---
+++ src/sept24/Lab010.java
@@ -0,0 +1,22 @@
+package sept24;
+
+public class Lab010 {
+    public static void main(String[] args)
+    {
+        int a=10;
+        System.out.println(a);
+        short b=20;
+        System.out.println(b);
+        long c=30;
+        System.out.println(c);
+        float d= 10.50f;
+        System.out.printf("%.2f", d);
+        System.out.println();
+        double e=50;
+        System.out.println(e);
+        char f ='A';
+        System.out.println(f);
+        double g=10.3;
+        System.out.println(g);
+    }
+}
---
+++ src/sept24/Lab011.java
@@ -0,0 +1,11 @@
+package sept24;
+
+public class Lab011 {
+    public static void main(String[] args) {
+        float d = 10.50f;
+        System.out.printf("%.2f", d);
+System.out.println();
+        float e= 10.50f;
+        System.out.printf("%.2f", e);
+    }
+}
