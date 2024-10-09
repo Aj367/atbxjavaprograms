8ae3ca4ebd234072dd0925177aff5fa757d66e93
Mansan <gitac164367@gmail.com>
2024-10-09 18:18:20 +0530

    modified

---
+++ src/sept24/Lab014.java
@@ -0,0 +1,8 @@
+package sept24;
+import org.openqa.selenium.WebDriver;
+public class Lab014 {
+    public static void main(String[] args) {
+        //webdriver driver =new chromedrivS:/Intellijfolder/chromedriver.exe);
+        System.out.println("driver");
+    }
+}
